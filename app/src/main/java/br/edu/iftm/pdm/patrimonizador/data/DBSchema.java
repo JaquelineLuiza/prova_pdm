8988e5620a356426ae90bde4106735101ff165c4
Jaqueline_Araujo <jaquelineluiza4444@gmail.com>
2020-08-16 13:32:43 -0300

    Inicializar repositorio

---
+++ app/src/main/java/br/edu/iftm/pdm/patrimonizador/data/DBSchema.java
@@ -0,0 +1,53 @@
+package br.edu.iftm.pdm.patrimonizador.data;
+
+public class DBSchema {
+
+    public static final class ImagemT {
+        public static final String TABELA = "imagem";
+        public static final String ID = "i_id";
+        public static final String PATH = "i_path";
+        public static final String PID = "i_pid";
+
+        public static final String getCreationQuery() {
+            // TODO (1) você deverá implementar a query de criação da tabela imagem OK
+            String sqlImagem = "CREATE TABLE IF NOT EXISTS" + TABELA + "("+ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                    PATH+" TEXT NOT NULL, "+
+                    PID+" TEXT NOT NULL);";
+            return sqlImagem;
+        }
+    }
+
+    public static final class PatrimonioT {
+
+        public static final String TABELA = "patrimonio";
+        public static final String VIEWSELECTION = "p_get_patrimonio";
+
+        public static final String ID = "p_id";
+        public static final String CATEGORIA = "p_categoria";
+        public static final String MARCA = "p_marca";
+        public static final String ESTADO = "p_estado";
+        public static final String VALOR = "p_valor";
+        public static final String DESCRICAO = "p_descricao";
+        public static final String TIMESTAMP = "p_timestamp";
+
+        public static final String getCreationQuery() {
+            // TODO (2) você deverá implementar a query de criação da tabela patrimonio OK
+            String sqlPatrimonio = "CREATE TABLE IF NOT EXISTS" + TABELA + "("+ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+
+                    CATEGORIA+" TEXT NOT NULL, " +
+                    MARCA+" TEXT NOT NULL, "+
+                    ESTADO+" TEXT NOT NULL, "+
+                    VALOR+" FLOAT, "+
+                    DESCRICAO+" TEXT NOT NULL, "+
+                    TIMESTAMP+" DATETIME DEFAULT CURRENT_TIMESTAMP);";
+            return sqlPatrimonio;
+        }
+
+        public static final String getViewCreationQuery() {
+            //TODO (3) você deverá implementar a query de criação de uma view de seleção para patrimonio.
+            // Nesta view você deverá criar a selecão da relação patrimonio-imagem. OK
+            return "CREATE VIEW IF NOT EXISTS " + VIEWSELECTION
+                    + " AS SELECT * FROM " + TABELA + " JOIN " + PatrimonioT.TABELA
+                    + " ON " + ID + " = " + ImagemT.PID + ";";
+        }
+    }
+}
---
+++ app/src/main/java/br/edu/iftm/pdm/patrimonizador/data/PatrimonioDAOSingleton.java
@@ -0,0 +1,110 @@
+package br.edu.iftm.pdm.patrimonizador.data;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import br.edu.iftm.pdm.patrimonizador.model.Patrimonio;
+import br.edu.iftm.pdm.patrimonizador.ui.activities.CadastraPatrimonioActivity;
+
+public class PatrimonioDAOSingleton {
+    private static PatrimonioDAOSingleton INSTANCE;
+    private DBHelper dbHelper;
+
+    private PatrimonioDAOSingleton() {
+        this.dbHelper = new DBHelper();
+    }
+
+    public static PatrimonioDAOSingleton getINSTANCE() {
+        if(INSTANCE == null)
+            INSTANCE = new PatrimonioDAOSingleton();
+        return INSTANCE;
+    }
+
+    public long addPatrimonio(Patrimonio patrimonio) {
+        //TODO (8) Neste método você deverá implementar a inserção de patrimônios
+        // e imagens no banco.
+        long resultado;
+        SQLiteDatabase db = this.dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DBSchema.PatrimonioT.CATEGORIA, patrimonio.getCategoria());
+        values.put(DBSchema.PatrimonioT.MARCA, patrimonio.getMarca());
+        values.put(DBSchema.PatrimonioT.ESTADO, patrimonio.getEstado());
+        values.put(DBSchema.PatrimonioT.VALOR, patrimonio.getValor());
+        values.put(DBSchema.PatrimonioT.DESCRICAO, patrimonio.getDescricao());
+
+        resultado = db.insert(DBSchema.PatrimonioT.TABELA, null, values);
+/*
+        if(resultado == -1){
+            Toast.makeText(CadastraPatrimonioActivity, "Erro ao inserir", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(CadastraPatrimonioActivity, "Registro inserido com sucesso", Toast.LENGTH_LONG).show();
+        }*/
+
+        db.close();
+
+        return 0;
+    }
+
+    public void deletePatrimonio(long id) {
+        //TODO (9) Implementar um método capaz de fazer a deleção de um patrimônio no banco SQLite.
+        SQLiteDatabase db = this.dbHelper.getWritableDatabase();
+        db.delete(DBSchema.PatrimonioT.TABELA, DBSchema.PatrimonioT.ID+" = ?",
+                new String[]{Long.toString(id)});
+        db.close();
+    }
+
+    public void deletePatrimonios(ArrayList<Long> idList) {
+        for(long id : idList) {
+            this.deletePatrimonio(id);
+        }
+    }
+
+    public Patrimonio getPatrimonio(long id) {
+        //TODO (10) Você deverá implementar um método capaz de buscar por um patrimônio
+        // de acordo com o seu id e retornar todos os seus dados.
+        SQLiteDatabase db = this.dbHelper.getWritableDatabase();
+        Cursor cursor = db.query(DBSchema.PatrimonioT.TABELA, null,
+                DBSchema.PatrimonioT.TABELA + " = ?",
+                new String[]{Long.toString(id)}, null, null, null);
+        cursor.moveToFirst();
+        long p_id = cursor.getLong(cursor.getColumnIndex(DBSchema.PatrimonioT.ID));
+        String p_categoria = cursor.getString(cursor.getColumnIndex(DBSchema.PatrimonioT.CATEGORIA));
+        String p_marca = cursor.getString(cursor.getColumnIndex(DBSchema.PatrimonioT.MARCA));
+        String p_estado = cursor.getString(cursor.getColumnIndex(DBSchema.PatrimonioT.ESTADO));
+        float p_valor = cursor.getFloat(cursor.getColumnIndex(DBSchema.PatrimonioT.VALOR));
+        String p_descricao = cursor.getString(cursor.getColumnIndex(DBSchema.PatrimonioT.DESCRICAO));
+        db.close();
+        return new Patrimonio(p_id, p_categoria, p_marca, p_estado, p_valor, p_descricao);
+    }
+
+    public ArrayList<Patrimonio> getAllPatrimonios() {
+        //TODO(11) Neste método você irá implementar o resgate da informação de todos os
+        // patrimônios do banco sqlite. Você deverá utilizar a VIEWSELECTION.
+        ArrayList<Patrimonio> patrimonios = new ArrayList<>();
+
+        SQLiteDatabase db = this.dbHelper.getWritableDatabase();
+        Cursor cursor = db.query(DBSchema.PatrimonioT.TABELA, null,
+                null,
+                null, null, null, null);
+        while (cursor.moveToNext()) {
+            long p_id = cursor.getLong(cursor.getColumnIndex(DBSchema.PatrimonioT.ID));
+            String p_categoria = cursor.getString(cursor.getColumnIndex(DBSchema.PatrimonioT.CATEGORIA));
+            String p_marca = cursor.getString(cursor.getColumnIndex(DBSchema.PatrimonioT.MARCA));
+            String p_estado = cursor.getString(cursor.getColumnIndex(DBSchema.PatrimonioT.ESTADO));
+            float p_valor = cursor.getFloat(cursor.getColumnIndex(DBSchema.PatrimonioT.VALOR));
+            String p_descricao = cursor.getString(cursor.getColumnIndex(DBSchema.PatrimonioT.DESCRICAO));
+            patrimonios.add(new Patrimonio(p_id, p_categoria, p_marca, p_estado, p_valor, p_descricao));
+        }
+        db.close();
+        return patrimonios;
+    }
+
+    public void updateEstado(long id, String estado) {
+        //TODO(12) você deverá implementar um método que altere a informação de estado
+        // do objeto por meio da informação de seu ID.
+    }
+}
